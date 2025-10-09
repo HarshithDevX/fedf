73b60667bd117b47f9a6b019b505b0289f1e46b0
HarshithDevX <2420080016@klh.edu.in>
2025-10-09 10:57:33 +0530

    Add files via upload

---
+++ week-7/data.js
@@ -0,0 +1,9 @@
+export const books = [
+  { title: "Attack on Titan", author: "Hajime Isayama", price: 350, availability: "in stock" },
+  { title: "Naruto", author: "Masashi Kishimoto", price: 400, availability: "out of stock" },
+  { title: "One Piece", author: "Eiichiro Oda", price: 500, availability: "in stock" },
+  { title: "Demon Slayer", author: "Koyoharu Gotouge", price: 450, availability: "in stock" },
+  { title: "My Hero Academia", author: "Kohei Horikoshi", price: 420, availability: "in stock" },
+  { title: "Death Note", author: "Tsugumi Ohba", price: 380, availability: "out of stock" },
+  {title: "Fullmetal Alchemist", author: "Hiromu Arakawa", price : 999, availability: "in stock"},
+];
